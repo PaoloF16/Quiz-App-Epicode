77d38219f1924f35a951353480131d049af48f8d
cafagnarob <cafagna.rob@gmail.com>
2026-03-24 15:34:28 +0100

    fix_results_list

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,19 @@
 // DOM Elements Declarations
 
-const questionTitle = document.getElementById("question-title");
-const buttonSpace = document.getElementById("button-space");
-const currentQuestionNum = document.getElementById("question-num");
-const buttonAnswers = document.querySelectorAll(".button-answer");
+const questionTitle = document.getElementById("question-title")
+const buttonSpace = document.getElementById("button-space")
+const currentQuestionNum = document.getElementById("question-num")
+const buttonAnswers = document.querySelectorAll(".button-answer")
 
 // Global Variables Delcaration
 
-let score = parseInt(sessionStorage.getItem("score")) || 0; // Dinamically updated score that will be displayed in the results page.
-let questionNumber = 0; // Number of the question the user is facing.
+let score = parseInt(sessionStorage.getItem("score")) || 0 // Dinamically updated score that will be displayed in the results page.
+let questionNumber = 0 // Number of the question the user is facing.
 const usedQuestionsArr =
-  JSON.parse(sessionStorage.getItem("usedQuestionsArr")) || [];
+  JSON.parse(sessionStorage.getItem("usedQuestionsArr")) || []
 const usedAnswersArr =
-  JSON.parse(sessionStorage.getItem("usedAnswersArr")) || [];
-let currentQuestion = {};
+  JSON.parse(sessionStorage.getItem("usedAnswersArr")) || []
+let currentQuestion = {}
 const questions = [
   {
     category: "Science: Computers",
@@ -108,170 +108,168 @@ const questions = [
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
-];
+]
 
-const pulledQuestions = []; // Array domande già poste
+const pulledQuestions = [] // Array domande già poste
 
 const randomQuestionExtraction = function () {
   //funzione per randomizzare domande
   if (pulledQuestions.length === questions.length) {
     //se l'array pulled question è uguale a quello delle question vuol dire che le domande sono finite
-    console.log("Hai risposto a tutte le domande!");
-    return null;
+    console.log("Hai risposto a tutte le domande!")
+    return null
   }
-  let indiceRand;
-  let domandaScelta;
+  let indiceRand
+  let domandaScelta
   do {
-    indiceRand = Math.floor(Math.random() * questions.length); //indice delle domande nell'array
-    domandaScelta = questions[indiceRand];
-  } while (pulledQuestions.includes(domandaScelta));
-  pulledQuestions.push(domandaScelta);
-  return domandaScelta;
-};
+    indiceRand = Math.floor(Math.random() * questions.length) //indice delle domande nell'array
+    domandaScelta = questions[indiceRand]
+  } while (pulledQuestions.includes(domandaScelta))
+  pulledQuestions.push(domandaScelta)
+  return domandaScelta
+}
 
 // Function to generate a random array to mix the possible answers' positions each time.
 
 const getRandomQuestionOrder = (questionObj) => {
-  const { type } = questionObj;
+  const { type } = questionObj
   if (type === "multiple") {
-    const positions = [0, 1, 2, 3];
-    positions.sort(() => Math.random() - 0.5);
+    const positions = [0, 1, 2, 3]
+    positions.sort(() => Math.random() - 0.5)
 
-    return positions;
+    return positions
   } else {
-    const positions = [0, 1];
-    positions.sort(() => Math.random() - 0.5);
+    const positions = [0, 1]
+    positions.sort(() => Math.random() - 0.5)
 
-    return positions;
+    return positions
   }
-};
+}
 
 // logica timer
-let timerInterval = null;
+let timerInterval = null
 
 const startTimer = () => {
-  const timer = document.querySelector(".timer");
-  const progress = document.querySelector(".progress");
+  const timer = document.querySelector(".timer")
+  const progress = document.querySelector(".progress")
 
-  let totalSec = 20;
-  let timeLeft = totalSec;
+  let totalSec = 20
+  let timeLeft = totalSec
 
-  const circumference = 2 * Math.PI * 90;
-  progress.style.strokeDasharray = circumference;
+  const circumference = 2 * Math.PI * 90
+  progress.style.strokeDasharray = circumference
 
-  clearInterval(timerInterval);
+  clearInterval(timerInterval)
 
-  timer.textContent = timeLeft;
-  progress.style.strokeDashoffset = 0;
+  timer.textContent = timeLeft
+  progress.style.strokeDashoffset = 0
 
   timerInterval = setInterval(() => {
-    --timeLeft;
-    timer.textContent = " remaining " + timeLeft + "seconds";
-    timer.classList.add("timer-text");
+    --timeLeft
+    timer.textContent = " remaining " + timeLeft + "seconds"
+    timer.classList.add("timer-text")
 
-    const offset = circumference - (timeLeft / totalSec) * circumference;
-    progress.style.strokeDashoffset = offset;
+    const offset = circumference - (timeLeft / totalSec) * circumference
+    progress.style.strokeDashoffset = offset
 
     if (timeLeft <= 0) {
-      clearInterval(timerInterval);
+      clearInterval(timerInterval)
 
-      progress.style.strokeDashoffset = circumference;
+      progress.style.strokeDashoffset = circumference
 
       setTimeout(() => {
-        checkAnswer(null, currentQuestion);
-      }, 20);
+        checkAnswer(null, currentQuestion)
+      }, 20)
     }
-  }, 1000);
-};
+  }, 1000)
+}
 
 // Funzione to display the current question. It creates button elements which on their onclick attribute, fire the checkAnswer function to validate the answer.
 
 const displayNextQuestion = (questionObj) => {
-  currentQuestion = questionObj;
-  buttonSpace.innerHTML = "";
+  currentQuestion = questionObj
+  buttonSpace.innerHTML = ""
   if (questionNumber >= 10) {
-    sessionStorage.setItem("score", score);
-    sessionStorage.setItem("usedAnswersArr", JSON.stringify(usedAnswersArr));
-    sessionStorage.setItem(
-      "usedQuestionsArr",
-      JSON.stringify(usedQuestionsArr),
-    );
+    sessionStorage.setItem("score", score)
+    sessionStorage.setItem("usedAnswersArr", JSON.stringify(usedAnswersArr))
+    sessionStorage.setItem("usedQuestionsArr", JSON.stringify(usedQuestionsArr))
     questionTitle.innerText = `The Quiz is over.\n
-    Go to your results!`;
+    Go to your results!`
     buttonSpace.innerHTML = `
         <a href="../html/results.html">
         <button>Results</button>
         </a>
-    `;
-    buttonSpace.classList.add("button-start");
-    return;
+    `
+    buttonSpace.classList.add("button-start")
+    return
   }
-  const { question, correct_answer, incorrect_answers } = questionObj;
-  const allAnswers = [...incorrect_answers, correct_answer];
-  questionTitle.innerText = `${question}`;
-  questionNumber++;
-  currentQuestionNum.innerText = `QUESTION ${questionNumber}`;
+  const { question, correct_answer, incorrect_answers } = questionObj
+  const allAnswers = [...incorrect_answers, correct_answer]
+  questionTitle.innerText = `${question}`
+  questionNumber++
+  currentQuestionNum.innerText = `QUESTION ${questionNumber}`
   getRandomQuestionOrder(questionObj).forEach((index) => {
     buttonSpace.innerHTML += `
     <button class="button-answer">
         ${allAnswers[index]}
     </button>
-    `;
-  });
-  const buttonAnswers = document.querySelectorAll(".button-answer");
+    `
+  })
+  const buttonAnswers = document.querySelectorAll(".button-answer")
   buttonAnswers.forEach((button) =>
     button.addEventListener("click", (e) => checkAnswer(e, questionObj)),
-  );
-  startTimer();
-};
+  )
+  startTimer()
+}
 
 // Function to check if the answer is correct. If it is, updates score by 1.
 
 const checkAnswer = (e, questionObj) => {
-  clearInterval(timerInterval);
+  clearInterval(timerInterval)
 
-  const { question, correct_answer } = questionObj;
+  const { question, correct_answer } = questionObj
   if (!e || !e.target) {
     usedAnswersArr.push(
       `You didn't answer the question ❌
       ${correct_answer} ✅`,
-    );
-    usedQuestionsArr.push(question);
-    const nextQuestionObj = randomQuestionExtraction();
-    currentQuestion = nextQuestionObj;
-    displayNextQuestion(nextQuestionObj);
-    console.log(usedAnswersArr, usedQuestionsArr);
-    return;
+    )
+    usedQuestionsArr.push(question)
+    const nextQuestionObj = randomQuestionExtraction()
+    currentQuestion = nextQuestionObj
+    displayNextQuestion(nextQuestionObj)
+    console.log(usedAnswersArr, usedQuestionsArr)
+    return
   }
   if (e.target.innerText && e.target.innerText === correct_answer) {
-    score++;
-    usedAnswersArr.push(`Your answer: ${correct_answer} ✅`);
+    score++
+    usedAnswersArr.push(`Your answer: ${correct_answer} ✅`)
   } else {
     usedAnswersArr.push(
-      `Your answer: ${e.target.innerText} ❌<br>Correct answer: ${correct_answer} ✅`,
-    );
+      `Your answer: ${e.target.innerText} ❌
+      Correct answer: ${correct_answer} ✅`,
+    )
   }
-  usedQuestionsArr.push(question);
-  const nextQuestionObj = randomQuestionExtraction();
-  currentQuestion = nextQuestionObj;
-  displayNextQuestion(nextQuestionObj);
-  console.log(usedAnswersArr, usedQuestionsArr);
-};
+  usedQuestionsArr.push(question)
+  const nextQuestionObj = randomQuestionExtraction()
+  currentQuestion = nextQuestionObj
+  displayNextQuestion(nextQuestionObj)
+  console.log(usedAnswersArr, usedQuestionsArr)
+}
 
 // Function to display the results
 
 const displayResults = () => {
   const correctPercentageP = document.getElementById(
     "percentage-correct-answers",
-  );
-  const resultMessage = document.getElementById("result-message");
-  const wrongPercentageP = document.getElementById("percentage-wrong-answers");
-  const correctAnswersP = document.getElementById("number-correct-answers");
-  const wrongAnswersP = document.getElementById("number-wrong-answers");
-  correctPercentageP.innerText = `${score.toFixed(1) * 10}%`;
-  wrongPercentageP.innerText = `${(10 - score).toFixed(1) * 10}%`;
-  correctAnswersP.innerText = `${score}/10 questions`;
-  wrongAnswersP.innerText = `${10 - score}/10 questions`;
+  )
+  const resultMessage = document.getElementById("result-message")
+  const wrongPercentageP = document.getElementById("percentage-wrong-answers")
+  const correctAnswersP = document.getElementById("number-correct-answers")
+  const wrongAnswersP = document.getElementById("number-wrong-answers")
+  correctPercentageP.innerText = `${score.toFixed(1) * 10}%`
+  wrongPercentageP.innerText = `${(10 - score).toFixed(1) * 10}%`
+  correctAnswersP.innerText = `${score}/10 questions`
+  wrongAnswersP.innerText = `${10 - score}/10 questions`
   if (score < 6) {
     resultMessage.innerHTML = `
     <h4>We're sorry!</h4>
@@ -280,33 +278,33 @@ const displayResults = () => {
     <p>You can try again later!</p>
     <p>Check your email (including promotion / spam folder)</p>
     </div>
-    `;
+    `
   }
-};
+}
 
 window.addEventListener("load", () => {
   if (document.getElementById("benchmark-body")) {
-    displayNextQuestion(randomQuestionExtraction());
-    return;
+    displayNextQuestion(randomQuestionExtraction())
+    return
   } else if (document.getElementById("results-body")) {
-    const checkButton = document.getElementById("button-check");
-    const checkSection = document.getElementById("check-section");
-    displayResults();
+    const checkButton = document.getElementById("button-check")
+    const checkSection = document.getElementById("check-section")
+    displayResults()
     checkButton.addEventListener("click", () => {
-      checkButton.setAttribute("disabled", "true");
+      checkButton.setAttribute("disabled", "true")
       usedQuestionsArr.forEach((ques, i) => {
         checkSection.innerHTML += `
     <div class="answer-check">  
       <p>${ques}</p>
       <p>${usedAnswersArr[i]}</p>
     </div> 
-       `;
-      });
-    });
-    sessionStorage.clear();
-    return;
+       `
+      })
+    })
+    sessionStorage.clear()
+    return
   }
-});
+})
 
 // 1. Configurazione dei dati
 const data = {
@@ -317,33 +315,33 @@ const data = {
       hoverOffset: 1,
     },
   ],
-};
+}
 
-const stars = document.getElementsByClassName("star");
-const starVuota = "/assets/emptyStar.svg";
-const starPiena = "/assets/star.svg";
+const stars = document.getElementsByClassName("star")
+const starVuota = "/assets/emptyStar.svg"
+const starPiena = "/assets/star.svg"
 
 for (let i = 0; i < stars.length; i++) {
   stars[i].addEventListener("mouseenter", function () {
     for (let j = 0; j < stars.length; j++) {
       if (j <= i) {
-        stars[j].src = starPiena;
+        stars[j].src = starPiena
       } else {
-        stars[j].src = starVuota;
+        stars[j].src = starVuota
       }
     }
-  });
+  })
 
   stars[i].addEventListener("mouseleave", function () {
     for (let k = 0; k < stars.length; k++) {
-      stars[k].src = starVuota;
+      stars[k].src = starVuota
     }
-  });
+  })
 
   stars[i].addEventListener("click", function () {
-    let voto = i + 1;
-    alert("Rating: " + voto);
-  });
+    let voto = i + 1
+    alert("Rating: " + voto)
+  })
 }
 // 3. Inizializzazione del grafico con il plugin
 const config = {
@@ -352,19 +350,19 @@ const config = {
   options: {
     cutout: "70%",
   },
-};
+}
 
 // Render del grafico
-const myChart = new Chart(document.getElementById("myDonutChart"), config);
+const myChart = new Chart(document.getElementById("myDonutChart"), config)
 
 // form di feedback
 
-const formFeedback = document.getElementById("feedback-form");
+const formFeedback = document.getElementById("feedback-form")
 
 formFeedback.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const feedbackValue = document.getElementById("feedback").value;
-  console.log("Feedback ricevuto:", feedbackValue);
-  alert("Grazie! Il tuo feedback è stato registrato.");
-  formFeedback.reset();
-});
+  e.preventDefault()
+  const feedbackValue = document.getElementById("feedback").value
+  console.log("Feedback ricevuto:", feedbackValue)
+  alert("Grazie! Il tuo feedback è stato registrato.")
+  formFeedback.reset()
+})
